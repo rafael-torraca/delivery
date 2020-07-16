298db3c5d74938dc34687e7b65ee72a847e4deeb
r.torraca <rafael.torraca19@gmail.com>
2020-07-16 19:56:18 -0300

    Add basic templates and debug toolbar

--- delivery/app.py
+++ delivery/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from delivery.ext import site
+from delivery.ext import toolbar
+from delivery.ext import config
 #from delivery.ext import admin
 
-
+# aqui dentro inicializa-se as extensões contidas na pasta ext.
+# o toolbar deve ser inicializado antes do site, para renderizar o restante da aplicacao
+# configuracoes sao colocadas aqui
 
 def create_app():
     app = Flask(__name__)
+    config.init_app(app)
+    toolbar.init_app(app)
     site.init_app(app)
     #admin.init_app(app)
     return app
--- delivery/ext/site/main.py
+++ delivery/ext/site/main.py
@@ -1,11 +1,20 @@
 from flask import Blueprint
 from flask import request, render_template
-
+from flask import current_app
 
 bp = Blueprint("site", __name__)
 
 @bp.route("/")
 def index():
-    return render_template(
-        "index.html",
-        name=request.args['name']) #return render_template("index.html", name="Alow") 
+    current_app.logger.debug("OI eu sou o erro!")
+    return render_template("index.html") #return render_template("index.html", name="Alow") name=request.args['name']
+
+
+@bp.route("/sobre")
+def about():
+    return render_template("about.html")
+
+
+@bp.route("/restaurantes")
+def restaurants():
+    return render_template("restaurants.html")
